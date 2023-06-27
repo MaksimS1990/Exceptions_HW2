2cb78d326f0aa267d3baea2ab41cdd23e9779749
MaksimS <sipatovGT@mail.ru>
2023-06-27 16:12:49 +0400

    add_HW_2

--- src/main/java/org/example/Task01.java
+++ src/main/java/org/example/Task01.java
@@ -1,2 +1,24 @@
-package org.example;public class Task01 {
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        inputFloat();
+    }
+    private static float inputFloat() {
+        String input = null;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Ввод дробного числа: ");
+                System.out.println(input = scanner.nextLine());
+                return Float.parseFloat(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Введены некорректные данные!!!");
+            }
+            System.out.println(input);
+        }
+    }
 }
--- src/main/java/org/example/Task02.java
+++ src/main/java/org/example/Task02.java
@@ -1,2 +1,26 @@
-package org.example;public class Task02 {
+package org.example;
+public class Task02 {
+//    try {
+//        int d = 0;
+//        double catchedRes1 = intArray[8] / d;
+//        System.out.println("catchedRes1 = " + catchedRes1);
+//    } catch (ArithmeticException e) {
+//        System.out.println("Catching exception: " + e);
+//    }
+
+    public static void main(String[] args) {
+        Task2();
+    }
+    private static void Task2() {
+        try {
+            int[] arr = {1, 2, 4, 5, 7, 8, 9, 3, 6};
+            int d = 0; // для получения резльтата без исключения, меняем 0 на любое другое значение;
+            double result = arr[8] / d; // для отработки второго исключения может либо уменьшит размер массива, либо увеличить индекс;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception - " + e);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Catching exception - " + e);
+        }
+    }
 }
